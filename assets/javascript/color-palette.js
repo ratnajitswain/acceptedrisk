10a49daa31c1be25b88d2461cb87fb31c2e6c03c
Ratnajit Swain <ratnajitswain@gmail.com>
2021-06-25 05:05:17 +0530

    first commit

--- assets/javascript/color-palette.js
+++ assets/javascript/color-palette.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('.color-palette-btn').on('click', function(){  
         $('.color-palette').toggle(500);
     })
+    $('.upload-logo').on('click', function(){
+        $('#logofile').click()
+    })
 })
