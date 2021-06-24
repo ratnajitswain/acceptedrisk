cec3e332fe290bd71b5e6de83d4c1ee466dfc16c
Ratnajit Swain <ratnajitswain@gmail.com>
2021-06-24 14:19:44 +0530

    first commit

---
+++ assets/javascript/navbar.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#navbarContainer').html(`
+    <nav class="navbar navbar-light bg-light container-fluid">
+        <div class="container">
+        <a class="navbar-brand d-inline-flex" href="#">
+            <div class="nav-text1"><img src="/assets/images/utils/Group 1.png"></div>
+             
+         <i class="bi bi-list nav-image"></i>
+        
+          
+        </a>
+        <div class="d-flex flex-row-reverse">
+            <img class="nav-imgtop" src="/assets/images/utils/Ellipse 1.png">
+            <div class="line nav-imgtop"></div>
+            <img class="nav-imgtop" src="/assets/images/utils/Group 127.png">
+            <img class="nav-imgtop" src="/assets/images/utils/Group 126.png">
+            
+        </div>
+        </div>
+      </nav>
+    
+    `)
+})
--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    
     $('#sidebar').html('')
 
     $('#sidebar').html(`
