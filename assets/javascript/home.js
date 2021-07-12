b28d65086f51a5edb48528aa2a300d971d533f18
Ratnajit Swain <ratnajitswain@gmail.com>
2021-07-12 14:44:22 +0530

    first commit

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,5 +1,5 @@
 $('.card-footer-options2').hide();
-$('.card-footer-home-button').on('click', function(){  
+$('.swiper-slide-active').on('click', function(){  
     $('.swiper-slide-active .card-footer-options1').hide();
     $('.swiper-slide-active .card-footer-options2').show();
 })
