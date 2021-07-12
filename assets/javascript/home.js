c9117243308ea37b2e97036fc826088bd29d0796
Ratnajit Swain <ratnajitswain@gmail.com>
2021-07-12 18:43:45 +0530

    first commit

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,10 +1,16 @@
+$(document).ready(function(){
 $('.card-footer-options2').hide();
-$('.swiper-slide-active').on('click', function(){  
-    $('.swiper-slide-active .card-footer-options1').hide();
-    $('.swiper-slide-active .card-footer-options2').show();
+$('.swiper-slide').on('click', function(){  
+    $('.swiper-slide-active .card-footer-options1').toggle(1000);
+    $('.swiper-slide-active .card-footer-options2').toggle(1000);
 })
 
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        $('.swiper-slide-active').click()
+    }
+});
 
    
-
+})
    
