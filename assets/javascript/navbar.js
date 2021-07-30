5b126b20a141ff8ee777e0e715ca792e7f9d433f
Ratnajit Swain <ratnajitswain@gmail.com>
2021-07-30 14:27:55 +0530

    first commit

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -22,12 +22,19 @@ $(document).ready(function () {
             <img class="nav-imgtop notification" src="/assets/images/utils/Group 127.png">
             <a href="messaging.html" target="_blank
             "><img class="nav-imgtop" src="/assets/images/utils/Group 126.png"></a>
+            <a href="#"
+            "><div class="nav-imgtop notificationSentiment" style="background:#1B6354;color:#00A811;border-radius:50%;width:35px;height:35px;padding:6px"><i class="bi bi-bar-chart-fill"></i></div></a>
         </div>
         </div>
       </nav>
+
+
+
+
+
       <div class="notificationCard">
       <div class="notification-rect shadow"></div>
-      <ul class="list-group p-3" id="notificationList">
+      <ul class="list-group notificationList p-3" id="notificationList">
       <li>
       <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
       <span class="notification-head">Twitter Centiment Report</span>
@@ -68,6 +75,60 @@ $(document).ready(function () {
       </div>
 
 
+
+
+
+
+      <div class="notificationCardSentiment">
+      <div class="notification-rect shadow"></div>
+      <ul class="list-group sentiment p-3" id="notificationListSentiment">
+      <li>
+      <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
+      <span class="notification-head">Twitter Centiment Report</span>
+      <br>
+      <span class="notification-msg">Your Twitter Centiment Report is Ready</span>
+      </a>
+      </li>
+      <li>
+      <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
+      <span class="notification-head">Twitter Centiment Report</span>
+      <br>
+      <span class="notification-msg">Your Twitter Centiment Report is Ready</span>
+      </a>
+      </li>
+      <li>
+      <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
+      <span class="notification-head">Twitter Centiment Report</span>
+      <br>
+      <span class="notification-msg">Your Twitter Centiment Report is Ready</span>
+      </a>
+      </li>
+      <li>
+      <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
+      <span class="notification-head">Twitter Centiment Report</span>
+      <br>
+      <span class="notification-msg">Your Twitter Centiment Report is Ready</span>
+      </a>
+      </li>
+      <li>
+      <a href="#"class="notification-reports " data-bs-toggle="modal" data-bs-target="#notificationModal">
+      <span class="notification-head">Twitter Centiment Report</span>
+      <br>
+      <span class="notification-msg">Your Twitter Centiment Report is Ready</span>
+      </a>
+      </li>
+      </ul>
+      <div id="loadMore1" onclick="loadMore1()" class="notification-rect shadow">Load More</div>
+      </div>
+
+
+
+
+
+
+
+
+
       <div class="modal fade" id="notificationModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -86,7 +147,7 @@ $(document).ready(function () {
       </div>
     </div>
 
-    <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="profileModal" style="color:black;" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -117,34 +178,55 @@ $(document).ready(function () {
     
 
     `);
-    $('#notificationModal').on('shown.bs.modal', function (e) {
-        $(".notificationCard").hide();
-      })
+  $(".modal").on("shown.bs.modal", function (e) {
+    $(".notificationCard").hide();
+    $(".notificationCardSentiment").hide();
+  });
   $(".notification").on("click", () => {
     $(".notificationCard").toggle(1000);
   });
 
-  $(".list-group li").hide();
-  $(".list-group li:lt(" + 1 + ")").show();
+  $("#notificationList li").hide();
+  $("#notificationList li:lt(" + 1 + ")").show();
 
-  
-  
+  $("#notificationListSentiment li").hide();
+  $("#notificationListSentiment li:lt(" + 1 + ")").show();
+
+  $(".notificationSentiment").on("click", () => {
+      
+    $(".notificationCardSentiment").toggle(1000);
+  });
+
+  $(".sentiment li").hide();
+  $(".sentiment li:lt(" + 1 + ")").show();
 });
 
+var x = 1;
 
-
-  var x = 1;
-  
 function loadMore() {
-    $(".list-group li").show();
-    $("#loadMore").attr("onclick", "showLess()");
-    $("#loadMore").html("Show Less");
-  }
+  $(".notificationList li").show();
+  $("#loadMore").attr("onclick", "showLess()");
+  $("#loadMore").html("Show Less");
+}
 
 function showLess() {
-    $("#loadMore").attr("onclick", "loadMore()");
-    $("#loadMore").html("Load More");
-    $(".list-group li")
-      .not(":lt(" + x + ")")
-      .hide();
-  }
+  $("#loadMore").attr("onclick", "loadMore()");
+  $("#loadMore").html("Load More");
+  $(".notificationList li")
+    .not(":lt(" + x + ")")
+    .hide();
+}
+
+function loadMore1() {
+  $("#notificationListSentiment li").show();
+  $("#loadMore1").attr("onclick", "showLess1()");
+  $("#loadMore1").html("Show Less");
+}
+
+function showLess1() {
+  $("#loadMore1").attr("onclick", "loadMore1()");
+  $("#loadMore1").html("Load More");
+  $("#notificationListSentiment li")
+    .not(":lt(" + x + ")")
+    .hide();
+}
